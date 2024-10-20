2a9093faeee36a460442da90d76818638a041b67
Marius Calin <marius.calin0699@gmail.com>
2024-10-20 12:58:01 +0300

    Complete LandingPage

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan("tiny"));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+  
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
---
+++ public/scripts/script.js
@@ -0,0 +1,60 @@
+const sidebar = $('#sidebar');
+const content = $('.side-content');
+const navButton = $('.sidebar-button');
+const profile = $('.navbar');
+const rightPanel = $('.dropdown_menu');
+const theme = $('.toggle-theme');
+
+content.on('click', function(){
+    content.removeClass('faded'); // Fadeout the content
+    sidebar.removeClass('active');
+    navButton.removeClass('change');
+});
+
+navButton.on('click', function(){
+    sidebar.toggleClass('active'); // Show or hide sidebar
+    content.toggleClass('faded');
+    navButton.toggleClass('change');
+});
+
+
+// profile.on('click', function(){
+//     rightPanel.toggleClass('open');
+// });
+
+profile.on('click', function(){
+    rightPanel.slideToggle();
+});
+
+
+
+// switch theme dark/light
+theme.on('click', function(){
+    sidebar.toggleClass('switch-mode');
+    $('.nav-link').toggleClass('theme');
+    theme.toggleClass('btn-tog');
+    $('.lead').toggleClass('first-active');
+    $('.webdev').toggleClass("webdev-active");
+    $('.btn-footer').toggleClass('btn-footer-theme');
+    rightPanel.toggleClass('drop-theme');
+    profile.toggleClass('navbar-theme')
+    $('.sidebars').toggleClass('sidebar-active');
+    $('.card-personal').toggleClass('card-personal-theme');
+    $('.sidename').toggleClass('sidename-theme');
+
+    // switches the favicon
+    if( $('.favicon-header').hasClass('favicon-header-theme') ){
+        $('.favicon-header').attr("src","images/favicon32.png")
+        $('.favicon-header').removeClass('favicon-header-theme');
+    }else{
+        $('.favicon-header').attr("src","images/re-favicon32.png")
+        $('.favicon-header').addClass('favicon-header-theme');
+    }
+});
+
+
+// typing animation - npm install typed.js
+var typed = new Typed('.webdev', {
+    strings: ['Web Development'],
+    typeSpeed: 50,
+});
