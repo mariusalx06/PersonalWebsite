cdb14886c66a66c09562f8eab82af99c6e25506b
Marius Calin <marius.calin0699@gmail.com>
2025-01-04 00:07:50 +0200

    initial styling for ograph and twitter card

--- app/layout.js
+++ app/layout.js
@@ -14,11 +14,11 @@ export const metadata = {
     title: "Marius Calin Portfolio",
     description:
       "Discover Marius's portfolio showcasing his full-stack web development skills, including both front-end and back-end expertise.",
-    url: "https://personal-website-nu-navy-83.vercel.app", // To be replaced when finished
+    url: "https://personal-website-nu-navy-83.vercel.app", // to be modified with the correct URL
     siteName: "Marius Calin Portfolio",
     images: [
       {
-        url: "https://personal-website-nu-navy-83.vercel.app/mclogo.png", // To be replaced when finished
+        url: "https://personal-website-nu-navy-83.vercel.app/mcLogoOpenGraph.png", // To be replaced when finished
         width: 1200,
         height: 630,
         alt: "Marius Calin Portfolio Thumbnail",
@@ -30,7 +30,8 @@ export const metadata = {
     title: "Marius Calin Portfolio",
     description:
       "Explore Marius's portfolio showcasing his full-stack web development expertise.",
-    image: "https://personal-website-nu-navy-83.vercel.app/mclogo.png", // To be replaced when finished
+    image:
+      "https://personal-website-nu-navy-83.vercel.app/mcLogoTwitterCard.png", // To be replaced when finished
   },
 };
 
@@ -41,10 +42,10 @@ const structuredDataHome = {
   name: "Marius Calin",
   jobTitle: "Full-Stack Web Developer",
   url: metadata.openGraph.url, // Use the url from metadata
-  image: "https://personal-website-nu-navy-83.vercel.app/mclogo.png", // To be replaced when finished
+  image: "https://personal-website-nu-navy-83.vercel.app/profile.jpg", // To be replaced when finished
   description:
     metadata.description ||
-    "Marius Calin is a full-stack web developer specializing in front-end and back-end technologies.",
+    "Marius Calin is a full-stack web developer specialized in front-end and back-end technologies.",
   skills: [
     "JavaScript",
     "Node.js",
@@ -55,6 +56,76 @@ const structuredDataHome = {
     "RESTful APIs",
   ],
   hobbies: ["Skiing", "Exploring Nature"],
+
+  education: [
+    {
+      "@type": "EducationOccupationalCredential",
+      credentialCategory: "Degree",
+      educationalCredentialAwarded:
+        "Bachelor of Computer Science in Web Development",
+      issuedBy: {
+        "@type": "CollegeOrUniversity",
+        name: "University of Craiova",
+      },
+      dateIssued: "2021-07-01",
+    },
+    {
+      "@type": "EducationOccupationalCredential",
+      credentialCategory: "Degree",
+      educationalCredentialAwarded: "Master of Computer Science",
+      issuedBy: {
+        "@type": "CollegeOrUniversity",
+        name: "University of Craiova",
+      },
+      dateIssued: "2023-07-01",
+    },
+    {
+      "@type": "EducationOccupationalCredential",
+      credentialCategory: "Course",
+      educationalCredentialAwarded: "Mastering Python",
+      issuedBy: {
+        "@type": "Organization",
+        name: "Udemy",
+      },
+      dateIssued: "2024-10-12",
+    },
+    {
+      "@type": "EducationOccupationalCredential",
+      credentialCategory: "Course",
+      educationalCredentialAwarded: "Mastering Web Development",
+      issuedBy: {
+        "@type": "Organization",
+        name: "Udemy",
+      },
+      dateIssued: "2025-01-15",
+    },
+    {
+      "@type": "EducationOccupationalCredential",
+      credentialCategory: "Course",
+      educationalCredentialAwarded: "Mastering React",
+      issuedBy: {
+        "@type": "Organization",
+        name: "Udemy",
+      },
+      dateIssued: "2025-01-15",
+    },
+  ],
+  workExample: [
+    {
+      "@type": "CreativeWork",
+      name: "Personal Portfolio Website",
+      url: "https://personal-website-nu-navy-83.vercel.app/", // to be modified with the correct URL
+      description:
+        "A personal portfolio website showcasing full-stack web development projects, skills, and experience.",
+      image:
+        "https://personal-website-nu-navy-83.vercel.app/websiteSnapshot.png", // to be modified with the final version
+      dateCreated: "2025-01-01",
+      publisher: {
+        "@type": "Person",
+        name: "Marius Calin",
+      },
+    },
+  ],
 };
 
 export default function RootLayout({ children }) {
