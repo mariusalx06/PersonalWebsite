1aa58117bbb006689a7a3b534daca74f6587da90
Marius Calin <marius.calin0699@gmail.com>
2025-01-08 01:26:52 +0200

    complete WelcomeContainer

--- app/components/About/InfoSection/InfoSection.jsx
+++ app/components/About/InfoSection/InfoSection.jsx
@@ -1,7 +1,55 @@
 import styles from "./InfoSection.module.css";
+
+const aboutMe = {
+  profession: "Junior Web Developer",
+  location: "Craiova, Romania",
+  languages: [
+    "English (Fluent)",
+    "Italian (Intermediate)",
+    "Romanian (Native)",
+  ],
+  goals: [
+    "Become a Senior Web Developer",
+    "Contribute to open-source projects",
+    "Build my own tech startup",
+  ],
+};
+
 export default function InfoSection() {
+  const formatJson = (obj) => {
+    return JSON.stringify(obj, null, 2)
+      .replace(/const/g, `<span class="${styles.keyword}">const</span>`) // Color `const`
+      .replace(/aboutMe/g, `<span class="${styles.variable}">aboutMe</span>`) // Color `aboutMe`
+      .replace(/{/g, `<span class="${styles.brace}">{</span>`) // Color `{`
+      .replace(/}/g, `<span class="${styles.brace}">}</span>`) // Color `}`
+      .replace(/"([^"]+)"/g, `<span class="${styles.string}">"$1"</span>`) // Color string values
+      .replace(/\[/g, `<span class="${styles.array}">[</span>`) // Color array brackets
+      .replace(/\]/g, `<span class="${styles.array}">]</span>`); // Color array brackets
+  };
+
   return (
     <section className={styles.info}>
+      <pre className={styles.codeBlock}>
+        <code
+          dangerouslySetInnerHTML={{
+            __html: `<span class="${
+              styles.keyword
+            }">const</span> <span class="${
+              styles.variable
+            }">aboutMe</span> = <span class="${
+              styles.brace
+            }">{</span>\n  profession: <span class="${styles.lightBlue}">"${
+              aboutMe.profession
+            }"</span>,\n  location: <span class="${styles.lightBlue}">"${
+              aboutMe.location
+            }"</span>,\n  languages: ${formatJson(
+              aboutMe.languages
+            )},\n  goals: ${formatJson(aboutMe.goals)}\n<span class="${
+              styles.brace
+            }">};</span>`,
+          }}
+        />
+      </pre>
       <div className={styles.about}>
         <p>
           Holding both a{" "}
--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 "use client";
 
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { PageContext } from "./context/pageContext";
+import { motion, AnimatePresence } from "framer-motion";
 
 import styles from "./page.module.css";
 
@@ -15,14 +16,42 @@ export default function Home() {
   const { currentPage, isMobileDevice, goToNextPage, goToPreviousPage } =
     useContext(PageContext);
 
+  const [animationDirection, setAnimationDirection] = useState("display");
+
+  const handleNextPage = () => {
+    setAnimationDirection("enter");
+    goToNextPage();
+  };
+
+  const handlePreviousPage = () => {
+    setAnimationDirection("exit");
+    goToPreviousPage();
+  };
+
+  const slideInVariants = {
+    enter: {
+      x: "100%",
+      opacity: 0,
+      transition: { duration: 0.5 },
+    },
+    exit: {
+      x: "-100%",
+      opacity: 0,
+      transition: { duration: 0.5 },
+    },
+    display: {
+      x: 0,
+      opacity: 1,
+      transition: { duration: 1 },
+    },
+  };
+
   return (
     <>
       {isMobileDevice ? (
-        // On mobile, display all sections stacked
         <>
           <WelcomeContainer />
           <AboutContainer />
-
           <section className={styles.studiesContainer}>Studies Section</section>
           <section className={styles.projectsContainer}>
             Projects Section
@@ -30,31 +59,41 @@ export default function Home() {
           <section className={styles.contactContainer}>Contact Section</section>
         </>
       ) : (
-        // On desktop, show one section at a time and provide navigation buttons
         <>
-          {currentPage === 0 && <WelcomeContainer />}
-          {currentPage === 1 && <AboutContainer />}
-          {currentPage === 2 && (
-            <section className={styles.studiesContainer}>
-              Studies Section
-            </section>
-          )}
-          {currentPage === 3 && (
-            <section className={styles.projectsContainer}>
-              Projects Section
-            </section>
-          )}
-          {currentPage === 4 && (
-            <section className={styles.contactContainer}>
-              Contact Section
-            </section>
-          )}
+          <AnimatePresence initial={false} mode="wait">
+            <motion.div
+              className={styles.pageContainer}
+              key={currentPage}
+              initial={animationDirection === "enter" ? "enter" : "exit"}
+              animate="display"
+              exit="exit"
+              variants={slideInVariants}
+            >
+              {currentPage === 0 && <WelcomeContainer />}
+              {currentPage === 1 && <AboutContainer />}
+              {currentPage === 2 && (
+                <section className={styles.studiesContainer}>
+                  Studies Section
+                </section>
+              )}
+              {currentPage === 3 && (
+                <section className={styles.projectsContainer}>
+                  Projects Section
+                </section>
+              )}
+              {currentPage === 4 && (
+                <section className={styles.contactContainer}>
+                  Contact Section
+                </section>
+              )}
+            </motion.div>
+          </AnimatePresence>
 
           <div className={styles.navigationButtons}>
-            <button onClick={goToPreviousPage} className={styles.navButton}>
+            <button onClick={handlePreviousPage} className={styles.navButton}>
               <WestIcon />
             </button>
-            <button onClick={goToNextPage} className={styles.navButton}>
+            <button onClick={handleNextPage} className={styles.navButton}>
               <EastIcon />
             </button>
           </div>
