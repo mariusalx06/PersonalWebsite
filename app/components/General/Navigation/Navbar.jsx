a67e5d0a34a126d46d676fdffa33f1c6aa62e16b
Marius Calin <marius.calin0699@gmail.com>
2025-01-08 18:08:25 +0200

    finished navbar

--- app/components/General/Navigation/Navbar.jsx
+++ app/components/General/Navigation/Navbar.jsx
@@ -1,72 +1,90 @@
 "use client";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { PageContext } from "@/app/context/pageContext";
 import HomeIcon from "@mui/icons-material/Home";
 import PersonIcon from "@mui/icons-material/Person";
 import SchoolIcon from "@mui/icons-material/School";
 import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
 import ContactMailIcon from "@mui/icons-material/ContactMail";
-import { motion } from "framer-motion";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   const { goToPage, currentPage, isMobileDevice } = useContext(PageContext);
 
+  if (isMobileDevice) {
+    return null;
+  }
+
   const handleClick = (pageIndex) => {
     goToPage(pageIndex);
   };
 
-  const mobilePositions = [-9, 52, 115, 182, 251];
-  const desktopPositions = [-6.5, 61, 132, 205, 281.5];
-
-  const positions = isMobileDevice ? mobilePositions : desktopPositions;
-
-  const activePosition = positions[currentPage];
-
   return (
     <nav className={styles.navigation}>
       <div className={styles.navButtons}>
-        <motion.div
-          className={styles.circle}
-          animate={{
-            x: activePosition,
-          }}
-          transition={{ type: "spring", stiffness: 300, damping: 30 }}
-        />
         <button
           onClick={() => handleClick(0)}
           className={currentPage === 0 ? styles.activeButton : ""}
         >
-          <HomeIcon />
-          Home
+          <div
+            className={`${styles.iconWrapper} ${
+              currentPage === 0 ? styles.iconActive : ""
+            }`}
+          >
+            <HomeIcon />
+          </div>
         </button>
+
         <button
           onClick={() => handleClick(1)}
           className={currentPage === 1 ? styles.activeButton : ""}
         >
-          <PersonIcon />
-          About
+          <div
+            className={`${styles.iconWrapper} ${
+              currentPage === 1 ? styles.iconActive : ""
+            }`}
+          >
+            <PersonIcon />
+          </div>
         </button>
+
         <button
           onClick={() => handleClick(2)}
           className={currentPage === 2 ? styles.activeButton : ""}
         >
-          <SchoolIcon />
-          Studies
+          <div
+            className={`${styles.iconWrapper} ${
+              currentPage === 2 ? styles.iconActive : ""
+            }`}
+          >
+            <SchoolIcon />
+          </div>
         </button>
+
         <button
           onClick={() => handleClick(3)}
           className={currentPage === 3 ? styles.activeButton : ""}
         >
-          <DeveloperModeIcon />
-          Projects
+          <div
+            className={`${styles.iconWrapper} ${
+              currentPage === 3 ? styles.iconActive : ""
+            }`}
+          >
+            <DeveloperModeIcon />
+          </div>
         </button>
+
         <button
           onClick={() => handleClick(4)}
           className={currentPage === 4 ? styles.activeButton : ""}
         >
-          <ContactMailIcon />
-          Contact
+          <div
+            className={`${styles.iconWrapper} ${
+              currentPage === 4 ? styles.iconActive : ""
+            }`}
+          >
+            <ContactMailIcon />
+          </div>
         </button>
       </div>
     </nav>
